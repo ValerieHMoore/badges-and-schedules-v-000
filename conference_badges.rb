a5423d5e530aafd05f456018b4c7bfeee39d6800
Valerie Moore <periwinkle17@outlook.com>
2018-06-09 15:29:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,11 +9,3 @@ def batch_badge_creator(attendees)
     end
   new_array
 end
-
-def square_array(array)
-    new_array = []
-    array.each do |element|
-        new_array << element ** 2
-    end
-    new_array
-end
