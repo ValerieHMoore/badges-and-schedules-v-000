1c624709b98b4c5f68d2e2cb88f62d12bccc79b7
Valerie Moore <periwinkle17@outlook.com>
2018-06-09 18:05:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,19 +10,19 @@ def batch_badge_creator(attendees)
   new_array
 end
 
-def assign_rooms(attendees)
-  attendees.each_with_index.map do |name, index|
-    "Hello, #{name}! You'll be assigned to room #{index +1}!"
-  end
-end
-
 # def assign_rooms(attendees)
-#   new_array = []
-#   attendees.each_with_index do |name, index|
-#     new_array << "Hello, #{name}! You'll be assigned to room #{index +1}!"
+#   attendees.each_with_index.map do |name, index|
+#     "Hello, #{name}! You'll be assigned to room #{index +1}!"
 #   end
-#   new_array
 # end
+
+def assign_rooms(attendees)
+  new_array = []
+  attendees.each_with_index do |name, index|
+    new_array << "Hello, #{name}! You'll be assigned to room #{index +1}!"
+  end
+  new_array
+end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
