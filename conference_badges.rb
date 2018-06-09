7dac2b6d0aece5ef91b9eea3479c1a751af349df
Valerie Moore <periwinkle17@outlook.com>
2018-06-09 15:29:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,15 @@ end
 def batch_badge_creator(attendees)
   new_array = []
   attendees.each do |name|
-  puts "Hello, my name is #{name}."
-  end
+    new_array << "Hello, my name is #{name}."
+    end
   new_array
+end
+
+def square_array(array)
+    new_array = []
+    array.each do |element|
+        new_array << element ** 2
+    end
+    new_array
 end
