d4ea15384c5bf3572934951a71a7a9bd75679878
Valerie Moore <periwinkle17@outlook.com>
2018-06-09 16:00:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 def assign_rooms(attendees)
   new_array = []
   attendees.each_with_index do |name,index|
-    new_array << "Hello, #{name}! You'll be assigned to room #{index +=1}!"
+    new_array << "Hello, #{name}! You'll be assigned to room #{index += 1}!"
   end
 end
