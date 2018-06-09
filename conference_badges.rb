c5976839d6c61f01a0232589c06d98e4bfba4fe7
Valerie Moore <periwinkle17@outlook.com>
2018-06-09 16:12:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,9 +18,9 @@ def assign_rooms(attendees)
   new_array
 end
 
-def printer
-  print batch_badge_creator(attendees)
-  print assign_rooms(attendees)
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
   end
   end
   end
