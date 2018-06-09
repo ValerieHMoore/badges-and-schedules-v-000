60f9dfb21df073b3d9843ec0e43703b72d58af0c
Valerie Moore <periwinkle17@outlook.com>
2018-06-09 17:59:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,14 @@ def assign_rooms(attendees)
   new_array
 end
 
+# def assign_rooms(attendees)
+#   new_array = []
+#   attendees.each_with_index do |name, index|
+#     new_array << "Hello, #{name}! You'll be assigned to room #{index +1}!"
+#   end
+#   new_array
+# end
+
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
